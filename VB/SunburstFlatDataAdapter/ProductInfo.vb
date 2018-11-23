e6a5f04c334a2a2d75794d1c7082a636aba2d797
DevExpressExampleBot <fakeEmail>
2018-11-23 14:29:10 +0300

    Source auto update [skip ci]

---
+++ VB/SunburstFlatDataAdapter/ProductInfo.vb
@@ -0,0 +1,14 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace SunburstFlatDataAdapterSample
+    Public Class ProductInfo
+        Public Property Product() As String
+        Public Property Category() As String
+        Public Property Country() As String
+        Public Property Annual() As Double
+    End Class
+End Namespace
